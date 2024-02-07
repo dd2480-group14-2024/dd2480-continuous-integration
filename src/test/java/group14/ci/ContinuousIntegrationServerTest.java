51bfcb7aae7067cc3d4d057bacd69030e74a2462
lvainio <lvainio@kth.se>
2024-02-07 08:40:46 +0100

    feat: Set up maven: Create the java file for server

---
+++ src/test/java/group14/ci/ContinuousIntegrationServerTest.java
@@ -0,0 +1,10 @@
+package group14.ci;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ContinuousIntegrationServerTest {
+    
+}
