aa6d62848554094572195d956d60e29d9c2f8241
lvainio <lvainio@kth.se>
2024-02-07 10:02:30 +0100

    test

--- src/main/java/group14/ci/ContinuousIntegrationServer.java
+++ src/main/java/group14/ci/ContinuousIntegrationServer.java
@@ -64,6 +64,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
             String branch = payload.getString("ref");
             System.out.println(repoUrl + " " + branch);
             Path repoPath = cloneRepository(repoUrl, branch);
+            System.out.println(repoPath.toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -95,6 +96,7 @@ public class ContinuousIntegrationServer extends AbstractHandler {
      * has been cloned to if the operation is successful or null if the operation failed. 
      */
     private Path cloneRepository(String repoUrl, String branch) {
+        System.out.println("In clone");
         try {
             Path repoPath = Files.createTempDirectory("ci-repo-");
             Git.cloneRepository()
